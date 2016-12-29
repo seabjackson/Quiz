a72530fa7d67e4cdf7469f840abbbbd79b68e502
Seab <honeyboyseab@gmail.com>
2016-12-29 14:55:23 -0500

    added IBAction methods and outlets for full functionality of the quiz app

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?",
+        "What is the capital of Taiwan?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montepelier",
+        "Grapes",
+        "Taipei"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
     
 }
+
+
+
+
+
+
+
+
+
+
+
 
