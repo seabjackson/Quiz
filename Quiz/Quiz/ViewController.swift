cee4f1cfbf0813bfb73d04f393b7fbd5d55aa3d5
Seab <honeyboyseab@gmail.com>
2016-12-29 14:28:50 -0500

    first commit where views were set up in interface builder

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Seab on 12/29/16.
+//  Copyright © 2016 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
